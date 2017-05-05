954dc9ecbcf0750c0ad7a6db6837c5ae920c3e66
Silas Reinagel <silas.reinagel@gmail.com>
2017-05-04 20:19:56 -0700

    Added map tile art. Closes #6

--- AmberSunrise/Program.cs
+++ AmberSunrise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AmberSunrise.Scenes;
 using Microsoft.Xna.Framework.Input;
 using MonoDragons.Core.EngimaDragons;
 using MonoDragons.Core.Engine;
@@ -14,7 +15,7 @@ namespace AmberSunrise
         [STAThread]
         static void Main()
         {
-            using (var game = new MainGame("Amber Sunrise", "Logo", new Display(960, 960, false), CreateSceneFactory(), CreateController()))
+            using (var game = new MainGame("Amber Sunrise", "Map1", new Display(960, 960, false), CreateSceneFactory(), CreateController()))
                 game.Run();
         }
 
@@ -28,7 +29,8 @@ namespace AmberSunrise
             return new SceneFactory(
                 new Dictionary<string, Func<IScene>>
                 {
-                    { "Logo", () => new LogoScene() }
+                    { "Logo", () => new LogoScene() },
+                    { "Map1", () => new Map1() },
                 });
         }
     }
---
+++ AmberSunrise/Scenes/Map1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.Entities;
+using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.Render;
+
+namespace AmberSunrise.Scenes
+{
+    public class Map1 : IScene
+    {
+        public void Init()
+        {
+            Entity.Create()
+                .Add(new ScreenBackgroundColor { Color = Color.Black });
+            for (var x = 0; x < 960; x += 96)
+                for (var y = 0; y < 960; y += 96)
+                    Entity.Create()
+                        .Add(new Spatial2(new Transform2(new Vector2(x, y), new Size2(96, 96))))
+                        .Add(new Sprite("Map/tile1"));
+
+        }
+
+        public void Update(TimeSpan delta) { }
+        public void Draw() { }
+    }
+}
